bb92ccb6f1787d80203004fa0660852de2795f51
Musheer <musheerhashmi47@gmail.com>
2025-06-24 23:57:55 +0530

    mongodb added

--- server.py
+++ server.py
@@ -6,6 +6,14 @@ from recipe_ing import recipe
 from listings import recipe_listings
 from urllib.parse import unquote
 from time import time
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+db = myclient["spark"]
+
+col = db["dish"]
+
 
 app = flask.Flask('test')
 CORS(app,allow_headers='*')
@@ -19,8 +27,18 @@ def show_recipe(dish):
 @app.route('/api/recipe_listings/<dish>',methods =['GET'])
 def recipe_links(dish):
     print(time())
+    query = list(col.find({'dish' : dish}))
+    
+    if len(query):
+        return query['json']
     result = recipe_listings(unquote(dish))
     print(time())
+    col.insert_one(
+        {
+            'dish' : dish,
+            'json' : result
+        }
+    )
     return result
 
 if __name__ == '__main__':
